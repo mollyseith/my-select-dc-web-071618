a06928ef72ac666d36e0bd7ebbf10d542142a6b6
Molly Seither <molly.seither@centre.edu>
2018-07-14 20:16:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
 def my_select(collection)
- # your code here!
+  ret = []
+  i = 0
+  while i < collection.length
+    ret.push yield(collection[i])
+    i += 1
+  end
+  ret
+end
+
+def my_select(array)
+  i=0
+  ret = []
+  while i < array.length
+    if (yield(array[i]))
+      ret << array[i]
+    end
+    i += 1
+  end
+  ret
 end
