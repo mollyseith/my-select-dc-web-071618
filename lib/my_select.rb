7e34b5721f7b206c0ad0ef3f75b3a08c02434bcc
Molly Seither <molly.seither@centre.edu>
2018-07-14 20:14:12 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  ret = []
+  i = 0
+  while i < collection.length
+    ret.push yield(collection[i])
+    i += 1
+  end
+  ret
 end
+
+my_select(nums) do |num|
+  if num.even?
+    num
+  end
+end
+
